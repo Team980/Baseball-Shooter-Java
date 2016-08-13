40f83985b110fbb9b50372c53c5a4488b8d1e670
Luke Myers <19lmyers@gmail.com>
2016-08-13 10:33:19 -0700

    Finalize shooting mechanism
    Relays should be plugged into the following channels:
    - 0 -> Winch
    - 1 -> Solenoid of shooter
    
    Buttons 3 and 4 should retract and release the winch.
    
    The code assumes that if a button is not being pressed the corresponding
    relay should stop the current.

--- src/com/team980/baseballshooter/Parameters.java
+++ src/com/team980/baseballshooter/Parameters.java
@@ -13,6 +13,10 @@ public class Parameters {
 	public static double autoDistance = 15.0; //feet
 	public static double autoSpeed = 1.0;
 	
+	// --- RELAYS ---
+	public static int winchRelayChannel = 0;
+	public static int solenoidRelayChannel = 1;
+	
 	// --- ENCODERS --- 
 	public static int leftDriveEncA = 0;
 	public static int leftDriveEncB = 1;
@@ -23,6 +27,8 @@ public class Parameters {
 	// --- JOYSTICKS / DRIVER INPUTS ---
 	public static int driveJsChannel = 0;
 	
+	public static int driveJsWinchPullButton = 3;
+	public static int driveJsWinchReleaseButton = 4;
 	public static int driveJsTriggerButton = 1;
 	public static int driveJsFailsafeButton = 9;
 }
--- src/com/team980/baseballshooter/Robot.java
+++ src/com/team980/baseballshooter/Robot.java
@@ -4,6 +4,7 @@ package com.team980.baseballshooter;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -13,6 +14,9 @@ public class Robot extends IterativeRobot {
 	
 	private Joystick driveStick;
 	
+	private Relay winchRelay;
+	private Relay solenoidRelay;
+	
 	private Encoder leftDriveEnc;
 	private Encoder rightDriveEnc;
 	
@@ -20,6 +24,9 @@ public class Robot extends IterativeRobot {
 		robotDrive = new RobotDrive(Parameters.leftDriveMotorChannel, Parameters.rightDriveMotorChannel);
 		
 		driveStick = new Joystick(Parameters.driveJsChannel);
+		
+		winchRelay = new Relay(Parameters.winchRelayChannel);
+		solenoidRelay = new Relay(Parameters.solenoidRelayChannel);
 		
 		leftDriveEnc = new Encoder(Parameters.leftDriveEncA, Parameters.leftDriveEncB);
 		//leftDriveEnc.setDistancePerPulse(null); TODO figure out the calculations for this
@@ -57,8 +64,23 @@ public class Robot extends IterativeRobot {
 
     public void teleopPeriodic() {
     	
+    	if (driveStick.getRawButton(Parameters.driveJsWinchPullButton)) {
+    		//Pull the winch
+    		winchRelay.set(Relay.Value.kReverse);
+    	} else if (driveStick.getRawButton(Parameters.driveJsWinchReleaseButton)) {
+    		//Release the winch
+    		winchRelay.set(Relay.Value.kForward);
+    	} else {
+    		//Do nothing with the winch
+    		winchRelay.set(Relay.Value.kOff);
+    	}
+    	
     	if (driveStick.getRawButton(Parameters.driveJsTriggerButton) && driveStick.getRawButton(Parameters.driveJsFailsafeButton)) {
-    		//Fire the cannon!
+    		//Fire the baseball!
+    		solenoidRelay.set(Relay.Value.kForward);
+    	} else {
+    		//Don't fire
+    		solenoidRelay.set(Relay.Value.kOff);
     	}
     	
         robotDrive.arcadeDrive(driveStick, Joystick.AxisType.kY.value, driveStick, Joystick.AxisType.kZ.value);
